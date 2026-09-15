adec334a7ad9a24c5f9176d4ee402cff8e8fbf7f
Thimesh Madusanka <thimesh.madusanka19@gmail.com>
2026-09-16 01:42:08 +1200

    modified

--- app/context/LanguageContext.tsx
+++ app/context/LanguageContext.tsx
@@ -11,7 +11,7 @@ import React, {
 
 const RAW_API_BASE =
   process.env.NEXT_PUBLIC_BACKEND_URL ||
-  "http://localhost:8000"; 
+  "https://web-production-3c6bc.up.railway.app/"; 
 const API_BASE = RAW_API_BASE.replace(/\/+$/, "");
 
 export type TrilingualTranslations = Record<string, string>;
